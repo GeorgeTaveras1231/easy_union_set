e33a317b3db4b1b076286068c7ddd52c21928f6d
GeorgeTaveras1231 <george.taveras1231@gmail.com>
2014-08-04 10:24:43 -0400

    commiting changed in main module

--- lib/easy_union_set.rb
+++ lib/easy_union_set.rb
@@ -1,2 +1,31 @@
 module EasyUnionSet
+  extend ActiveSupport::Concern
+
+  module GroupMethods
+
+    def & ar_rel
+      set = self.intersect(ar_rel)
+      table_alias = arel_table.create_table_alias(set, table_name)
+
+      ancestors.first.from(table_alias)
+    end
+
+    def | ar_rel
+      union = case ar_rel
+              when ActiveRecord::Relation
+                self.union(ar_rel)
+              when Hash
+                ar_rel.assert_valid_keys(:all)
+                self.union(:all, ar_rel[:all])          
+              end
+      
+      table_alias = arel_table.create_table_alias(union, table_name)
+
+      ancestors.first.from(table_alias)
+    end
+  end
+
 end
+
+
+ActiveRecord::Relation.send(:include, EasyUnionSet::GroupMethods)
